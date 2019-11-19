117924a57784f9c96b8447c8caf4cd3633a357f9
Tobias Bartz <tobias.bartz@graypenguins.eu>
2019-11-19 15:57:55 +0100

    Test

--- templates/startpage/assets/js/clock.js
+++ templates/startpage/assets/js/clock.js
@@ -2,7 +2,7 @@ function start() {
     time();
     window.setInterval("time()", 1000);
 }
-
+// Test
 function time() {
     var now = new Date();
     var dayname = ['Sonntag','Montag','Dienstag','Mittwoch','Donnerstag','Freitag','Samstag' ];
