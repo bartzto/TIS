8d30dc1e68668caa8357d9dcc187f9a3d5afe1a2
Lukas Holzner <meistergig@web.de>
2019-11-27 15:29:35 +0100

    Added Database

--- engine/dbreader.js
+++ engine/dbreader.js
@@ -1,17 +1,31 @@
-const sqlite3 = require('sqlite3').verbose();
- 
-// open database in memory
-let db = new sqlite3.Database(':memory:', (err) => {
-  if (err) {
-    return console.error(err.message);
-  }
-  console.log('Connected to the in-memory SQlite database.');
-});
- 
-// close the database connection
-db.close((err) => {
-  if (err) {
-    return console.error(err.message);
-  }
-  console.log('Close the database connection.');
-});
+//const sqlite3 = require('sqlite3').verbose();
+var sqlite = require("better-sqlite3");
+var root = "/home/lukas/development/electron/TIS" 
+// open database in memor
+//var db = new sqlite("example.db");
+var db = new sqlite(root+"/database/tis.db");
+function test(){
+  
+  var rows = db.prepare("SELECT date FROM `tis_room`;").get();
+  return rows;
+}
+
+
+function getAllUsers(){
+  var querry = 'SELECT * FROM `tis_user`;'
+  return db.prepare(querry).all();
+}
+
+function getBookedBy(date, isafternoon){
+  var name = null;
+  var querry = 'SELECT '
+
+  return name;
+}
+
+function getSatus(date,isafternoon){
+  
+  return status; // {status: 0; name: "Lukas Holzner"; reserveduntil: "29.11.2019"}
+}
+
+console.log(test().date);
