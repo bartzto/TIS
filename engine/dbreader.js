b73275fc6bd3e39cae010a7495d3bcec26ebbcb6
Lukas Holzner <meistergig@web.de>
2019-11-27 16:55:46 +0100

    Added Function getBookedBy

--- engine/dbreader.js
+++ engine/dbreader.js
@@ -4,10 +4,13 @@ var root = "/home/lukas/development/electron/TIS"
 // open database in memor
 //var db = new sqlite("example.db");
 var db = new sqlite(root+"/database/tis.db");
-function test(){
-  
-  var rows = db.prepare("SELECT date FROM `tis_room`;").get();
-  return rows;
+
+function formatDate (date){
+  console.log(date);
+  return date.getFullYear()+'-'+(date.getMonth()+1)+'-'+date.getDate();
+}
+function formatName (name){
+  return name.vorname + " " +name.nachname;
 }
 
 
@@ -17,10 +20,13 @@ function getAllUsers(){
 }
 
 function getBookedBy(date, isafternoon){
-  var name = null;
-  var querry = 'SELECT '
-
-  return name;
+  var querry = 'SELECT vorname, nachname \
+    FROM tis_room, tis_user \
+    WHERE tis_user.user = tis_room.user\
+    AND tis_room.isafternoon = ' + isafternoon+' \
+    AND tis_room.date = "'+ formatDate(date)+'";';
+  console.log(querry);
+  return formatName(db.prepare(querry).get());
 }
 
 function getSatus(date,isafternoon){
@@ -28,4 +34,4 @@ function getSatus(date,isafternoon){
   return status; // {status: 0; name: "Lukas Holzner"; reserveduntil: "29.11.2019"}
 }
 
-console.log(test().date);
+console.log(getBookedBy(new Date("2019-11-22"),true));
