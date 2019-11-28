ede668b2dd951331f9ea709ec7a09baf294cc551
Lukas Holzner <meistergig@web.de>
2019-11-28 13:10:00 +0100

    Added Status Database Connection

--- engine/dbreader.js
+++ engine/dbreader.js
@@ -6,7 +6,6 @@ var root = "/home/lukas/development/electron/TIS"
 var db = new sqlite(root+"/database/tis.db");
 
 function formatDate (date){
-  console.log(date);
   return date.getFullYear()+'-'+(date.getMonth()+1)+'-'+date.getDate();
 }
 function formatName (name){
@@ -19,19 +18,43 @@ function getAllUsers(){
   return db.prepare(querry).all();
 }
 
-function getBookedBy(date, isafternoon){
+function getBookedBy(date, isafternoon,room){
   var querry = 'SELECT vorname, nachname \
     FROM tis_room, tis_user \
     WHERE tis_user.user = tis_room.user\
-    AND tis_room.isafternoon = ' + isafternoon+' \
+    AND tis_room.isafternoon = ' + isafternoon +' \
+    AND tis_room.roomnr = "' + room+'" \
     AND tis_room.date = "'+ formatDate(date)+'";';
-  console.log(querry);
-  return formatName(db.prepare(querry).get());
+  var name = db.prepare(querry).get();
+  name = name != null ? formatName(name) : null;
+
+  return name;
 }
 
-function getSatus(date,isafternoon){
-  
+function getReserverdUntil(date,isafternoon,room){
+  console.log(date)
+  if(getBookedBy(date,isafternoon,room) == null){
+    date.setDate(date.getDate() - 1); 
+    return date;
+  } else {
+    if(isafternoon){
+      date.setDate(date.getDate() + 1);
+      return getReserverdUntil(date,false,room)
+    }else{
+      return getReserverdUntil(date,true,room)
+    }
+  }
+}
+
+function getStatus(date,isafternoon,room){
+  var getname = getBookedBy(date,isafternoon,room);
+  var status = {status: 0};
+  if(getname != null){
+    status.name = getname;
+    status['status'] = 1;
+    status['reserverduntil'] = getReserverdUntil(date,isafternoon,room);
+  }
   return status; // {status: 0; name: "Lukas Holzner"; reserveduntil: "29.11.2019"}
 }
 
-console.log(getBookedBy(new Date("2019-11-22"),true));
+console.log(getStatus(new Date("2019-11-28"),true, "207b"));
